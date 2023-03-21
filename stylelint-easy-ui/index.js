57afb4e1065527070a15f8dbef7b3555c1b7a2fd
ssingh <ssingh@easypost.com>
2023-03-21 11:27:41 -0700

    remove rule

--- stylelint-easy-ui/index.js
+++ stylelint-easy-ui/index.js
@@ -6,7 +6,6 @@ module.exports = {
   ],
   rules: {
     "scss/comment-no-empty": null,
-    "scss/at-extend-no-missing-placeholder": null,
     "scss/dollar-variable-pattern": [/^ezui-/, { ignore: "local" }],
   },
 };
