e98d996446cecc2ee238b396cacefaa23ada74ac
ssingh <ssingh@easypost.com>
2023-05-03 12:28:45 -0700

    creates unstyledbutton

---
+++ easy-ui-react/src/UnstyledButton/UnstyledButton.test.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Icon } from "../Icon";
+import CheckCircleIcon from "@easypost/easy-ui-icons/CheckCircle";
+import { UnstyledButton } from "./UnstyledButton";
+
+describe("<UnstyledButton />", () => {
+  it("should render an unstyled button", () => {
+    render(<UnstyledButton />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("should render an unstyled button with text", () => {
+    render(<UnstyledButton>Testing</UnstyledButton>);
+    expect(screen.getByText(/testing/i)).toBeInTheDocument();
+  });
+
+  it("should render an anchor tag", () => {
+    render(
+      <UnstyledButton href="https://www.easypost.com/">Testing</UnstyledButton>,
+    );
+    expect(screen.getByText(/testing/i).closest("a")).toHaveAttribute(
+      "href",
+      "https://www.easypost.com/",
+    );
+  });
+
+  it("should render an unstyled button with an icon", () => {
+    render(
+      <UnstyledButton>
+        <Icon symbol={CheckCircleIcon} />
+      </UnstyledButton>,
+    );
+    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.getByRole("img", { hidden: true })).toBeInTheDocument();
+  });
+
+  it("should apply the supplied class", () => {
+    render(
+      <UnstyledButton className="colorPrimary_123">Button</UnstyledButton>,
+    );
+    expect(screen.getByRole("button")).toHaveAttribute(
+      "class",
+      expect.stringContaining("colorPrimary"),
+    );
+  });
+
+  it("should apply the disabled attribute", () => {
+    render(<UnstyledButton isDisabled />);
+    expect(screen.getByRole("button")).toHaveAttribute("disabled");
+  });
+});
