2726cf27c11a2165ec8d57f072aa3cf2fec0cf5d
Stephen Watkins <swatkins@easypost.com>
2023-05-16 17:32:12 -0400

    Initial test

--- easy-ui-react/src/Badge/Badge.test.tsx
+++ easy-ui-react/src/Badge/Badge.test.tsx
@@ -1,5 +1,27 @@
+import Anchor from "@easypost/easy-ui-icons/Anchor";
+import { screen } from "@testing-library/react";
+import React from "react";
+import { render } from "../utilities/test";
+import { Badge } from "./Badge";
+import { vi } from "vitest";
+import noop from "lodash/noop";
+
 describe("<Badge />", () => {
-  it("should render", async () => {
+  it("should render simple text", () => {
+    render(<Badge>Badge text</Badge>);
+    expect(screen.getByText("Badge text")).toBeInTheDocument();
+  });
+
+  it("should render simple icon", () => {
+    render(<Badge accessibilityLabel="Intent of badge" icon={Anchor} />);
     expect(true).toBe(true);
+  });
+
+  it("should warn with no accessibility label on icon", () => {
+    const consoleWarningSpy = vi
+      .spyOn(console, "warn")
+      .mockImplementation(noop);
+    render(<Badge icon={Anchor} />);
+    expect(consoleWarningSpy).toBeCalled();
   });
 });
