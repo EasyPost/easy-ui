8b5f0a69bdf68ae9935e62849a01580063dd94e6
Stephen Watkins <swatkins@easypost.com>
2023-07-20 15:41:20 -0400

    fix copy button tooltip

--- easy-ui-react/src/CodeBlock/CopyButton.tsx
+++ easy-ui-react/src/CodeBlock/CopyButton.tsx
@@ -1,9 +1,10 @@
 import ContentCopyIcon from "@easypost/easy-ui-icons/ContentCopy";
 import React, { useCallback } from "react";
+import { mergeProps, useFocus, useHover } from "react-aria";
+import { useTooltipTriggerState } from "react-stately";
 import { useClipboard } from "use-clipboard-copy";
 import { Icon } from "../Icon";
 import { Tooltip } from "../Tooltip";
-import { UnstyledButton } from "../UnstyledButton";
 
 import styles from "./CopyButton.module.scss";
 
@@ -11,26 +12,48 @@ export type CopyButtonProps = {
   text: string;
 };
 
-export function CopyButton(props: CopyButtonProps) {
-  const { text } = props;
-
+export function CopyButton({ text }: CopyButtonProps) {
   const clipboard = useClipboard({ copiedTimeout: 2000 });
-
+  const { tooltipState, triggerProps } = useCopyButtonTooltipState();
   const handlePress = useCallback(() => {
-    console.log("here");
     clipboard.copy(text);
-  }, [clipboard, text]);
-
+    tooltipState.close();
+  }, [clipboard, text, tooltipState]);
   return (
     <Tooltip
-      isOpen={clipboard.copied ? true : undefined}
+      isOpen={tooltipState.isOpen || clipboard.copied}
       content={clipboard.copied ? "Copied!" : "Copy code block"}
     >
-      <UnstyledButton className={styles.CopyButton} onPress={handlePress}>
+      <button
+        className={styles.CopyButton}
+        onClick={handlePress}
+        {...triggerProps}
+      >
         <Icon symbol={ContentCopyIcon} />
-      </UnstyledButton>
+      </button>
     </Tooltip>
   );
 }
 
 CopyButton.displayName = "CopyButton";
+
+function useCopyButtonTooltipState() {
+  const tooltipState = useTooltipTriggerState();
+  const { hoverProps } = useHover({
+    onHoverStart() {
+      tooltipState.open();
+    },
+    onHoverEnd() {
+      tooltipState.close();
+    },
+  });
+  const { focusProps } = useFocus({
+    onFocus() {
+      tooltipState.open();
+    },
+    onBlur() {
+      tooltipState.close();
+    },
+  });
+  return { triggerProps: mergeProps(hoverProps, focusProps), tooltipState };
+}
