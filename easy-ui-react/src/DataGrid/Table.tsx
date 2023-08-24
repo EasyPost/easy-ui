a53e73912edc0b9a6921ca0f3cb2a5db5324fa23
Stephen Watkins <swatkins@easypost.com>
2023-08-24 14:30:29 -0400

    add intersection for detecting stuck elements

--- easy-ui-react/src/DataGrid/Table.tsx
+++ easy-ui-react/src/DataGrid/Table.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties, useRef } from "react";
 import { useTable } from "react-aria";
 import { useTableState } from "react-stately";
+import { useIntersectionDetection } from "../Modal/useIntersectionDetection";
 import { classNames, getComponentToken, variationName } from "../utilities/css";
 import { Cell } from "./Cell";
 import { ColumnHeader } from "./ColumnHeader";
@@ -30,6 +31,7 @@ export function Table<C extends Column>(props: DataGridProps<C>) {
     templateColumns,
   } = props;
 
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const tableRef = useRef<HTMLDivElement | null>(null);
   const state = useTableState({
     ...props,
@@ -46,6 +48,22 @@ export function Table<C extends Column>(props: DataGridProps<C>) {
   const { expandedRow, expandedRowStyle } = useExpandedRow({ tableRef, state });
   const { gridTemplateStyle } = useGridTemplate({ templateColumns, state });
 
+  const headerInterceptorRef = useRef<HTMLDivElement | null>(null);
+  const isHeaderUnderScroll = useIntersectionDetection(
+    headerInterceptorRef,
+    containerRef,
+  );
+  const leftColumnInterceptorRef = useRef<HTMLDivElement | null>(null);
+  const isLeftEdgeUnderScroll = useIntersectionDetection(
+    leftColumnInterceptorRef,
+    containerRef,
+  );
+  const rightColumnInterceptorRef = useRef<HTMLDivElement | null>(null);
+  const isRightEdgeUnderScroll = useIntersectionDetection(
+    rightColumnInterceptorRef,
+    containerRef,
+  );
+
   const { collection } = state;
   const { columns } = collection;
 
@@ -59,6 +77,9 @@ export function Table<C extends Column>(props: DataGridProps<C>) {
     hasSelection && styles.hasSelection,
     hasExpansion && styles.hasExpansion,
     hasRowActions && styles.hasRowActions,
+    isHeaderUnderScroll && styles.headerUnderScroll,
+    isLeftEdgeUnderScroll && styles.leftEdgeUnderScroll,
+    isRightEdgeUnderScroll && styles.rightEdgeUnderScroll,
   );
 
   const style = {
@@ -68,42 +89,78 @@ export function Table<C extends Column>(props: DataGridProps<C>) {
   } as CSSProperties;
 
   return (
-    <div {...gridProps} ref={tableRef} className={className} style={style}>
-      <RowGroup>
-        {collection.headerRows.map((headerRow) => (
-          <HeaderRow key={headerRow.key} item={headerRow} state={state}>
-            {[...headerRow.childNodes].map((column) =>
-              column.props.isSelectionCell ? (
-                <SelectAllColumnHeader
-                  key={column.key}
-                  column={column}
-                  state={state}
-                />
-              ) : (
-                <ColumnHeader key={column.key} column={column} state={state} />
-              ),
-            )}
-          </HeaderRow>
-        ))}
-      </RowGroup>
-      <RowGroup>
-        {[...collection.body.childNodes].map((row) => (
-          <Row key={row.key} item={row} state={state}>
-            {[...row.childNodes].map((cell) => {
-              return cell.props.isSelectionCell ? (
-                <SelectCell key={cell.key} cell={cell} state={state} />
-              ) : (
-                <Cell key={cell.key} cell={cell} state={state} />
-              );
-            })}
-          </Row>
-        ))}
-      </RowGroup>
-      {expandedRow && renderExpandedRow && (
-        <ExpandedRowContent>
-          {renderExpandedRow(expandedRow.key)}
-        </ExpandedRowContent>
-      )}
+    <div ref={containerRef} className={styles.container} style={style}>
+      <div {...gridProps} ref={tableRef} className={className}>
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 1,
+          }}
+          ref={headerInterceptorRef}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            width: 1,
+          }}
+          ref={leftColumnInterceptorRef}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            width: 1,
+          }}
+          ref={rightColumnInterceptorRef}
+        />
+        <RowGroup>
+          {collection.headerRows.map((headerRow) => (
+            <HeaderRow key={headerRow.key} item={headerRow} state={state}>
+              {[...headerRow.childNodes].map((column) =>
+                column.props.isSelectionCell ? (
+                  <SelectAllColumnHeader
+                    key={column.key}
+                    column={column}
+                    state={state}
+                  />
+                ) : (
+                  <ColumnHeader
+                    key={column.key}
+                    column={column}
+                    state={state}
+                  />
+                ),
+              )}
+            </HeaderRow>
+          ))}
+        </RowGroup>
+        <RowGroup>
+          {[...collection.body.childNodes].map((row) => (
+            <Row key={row.key} item={row} state={state}>
+              {[...row.childNodes].map((cell) => {
+                return cell.props.isSelectionCell ? (
+                  <SelectCell key={cell.key} cell={cell} state={state} />
+                ) : (
+                  <Cell key={cell.key} cell={cell} state={state} />
+                );
+              })}
+            </Row>
+          ))}
+        </RowGroup>
+        {expandedRow && renderExpandedRow && (
+          <ExpandedRowContent>
+            {renderExpandedRow(expandedRow.key)}
+          </ExpandedRowContent>
+        )}
+      </div>
     </div>
   );
 }
