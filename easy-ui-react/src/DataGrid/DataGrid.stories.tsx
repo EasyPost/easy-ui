265bbe56296d55c096cbe4fdbb36fadddb32239a
Stephen Watkins <swatkins@easypost.com>
2023-08-30 15:42:14 -0400

    remove story

--- easy-ui-react/src/DataGrid/DataGrid.stories.tsx
+++ easy-ui-react/src/DataGrid/DataGrid.stories.tsx
@@ -347,57 +347,6 @@ export const HeaderVariant: Story = {
   },
 };
 
-export const SingleSelectionModeWithDisabledKeys: Story = {
-  render: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const columns = useMemo(
-      () => [
-        { key: "name", name: "Name" },
-        { key: "type", name: "Type" },
-        { key: "date", name: "Date Modified" },
-      ],
-      [],
-    );
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const rows = useMemo(
-      () => [
-        {
-          key: 1,
-          name: "Games",
-          date: "6/7/2020",
-          type: "File folder with really long text let's see what happens",
-        },
-        {
-          key: 2,
-          name: "Program Files",
-          date: "4/7/2021",
-          type: "File folder",
-        },
-        { key: 3, name: "bootmgr", date: "11/20/2010", type: "System file" },
-        { key: 4, name: "log.txt", date: "1/18/2016", type: "Text Document" },
-      ],
-      [],
-    );
-
-    return (
-      <DataGrid
-        aria-label="Example dynamic collection table"
-        columns={columns}
-        rows={rows}
-        disabledKeys={[3]}
-        selectionMode="single"
-        renderColumnCell={(column) => (
-          <span style={{ whiteSpace: "nowrap" }}>{column.name}</span>
-        )}
-        renderRowCell={(item) => (
-          <span style={{ whiteSpace: "nowrap" }}>{item as string}</span>
-        )}
-      />
-    );
-  },
-};
-
 export const WithSort: Story = {
   render: () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
