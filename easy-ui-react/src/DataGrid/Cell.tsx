6a27709ca150ffe0179d0b74e57912a9e10a4e04
Stephen Watkins <swatkins@easypost.com>
2023-09-05 11:11:03 -0400

    clean up cell

--- easy-ui-react/src/DataGrid/Cell.tsx
+++ easy-ui-react/src/DataGrid/Cell.tsx
@@ -1,18 +1,17 @@
 import { GridNode } from "@react-types/grid";
-import React, { forwardRef, useRef } from "react";
+import React, { useRef } from "react";
 import { mergeProps, useFocusRing, useTableCell } from "react-aria";
 import { TableState } from "react-stately";
 import { classNames } from "../utilities/css";
 
 import styles from "./DataGrid.module.scss";
-import { mergeRefs } from "@react-aria/utils";
 
 type CellProps<T = unknown> = {
   cell: GridNode<T>;
   state: TableState<T>;
 };
 
-export const Cell = forwardRef<null, CellProps>(({ cell, state }, inRef) => {
+export function Cell({ cell, state }: CellProps) {
   const ref = useRef(null);
   const { gridCellProps } = useTableCell({ node: cell }, state, ref);
   const { isFocusVisible, focusProps } = useFocusRing();
@@ -22,13 +21,11 @@ export const Cell = forwardRef<null, CellProps>(({ cell, state }, inRef) => {
   );
   return (
     <div
+      ref={ref}
       className={className}
       {...mergeProps(gridCellProps, focusProps)}
-      ref={mergeRefs(ref, inRef)}
     >
       {cell.rendered}
     </div>
   );
-});
-
-Cell.displayName = "Cell";
+}
