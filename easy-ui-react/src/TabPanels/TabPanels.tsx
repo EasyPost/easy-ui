0be49bfdfa06fac8d3e6fe0e343ba9fd51ffa494
Stephen Watkins <swatkins@easypost.com>
2023-09-26 13:36:26 -0400

    fix type errors

--- easy-ui-react/src/TabPanels/TabPanels.tsx
+++ easy-ui-react/src/TabPanels/TabPanels.tsx
@@ -1,6 +1,8 @@
-import { AriaLabelingProps } from "@react-types/shared";
+import { ListCollection } from "@react-stately/list";
+import { AriaLabelingProps, ItemElement, Node } from "@react-types/shared";
 import React, {
   Key,
+  ReactElement,
   ReactNode,
   createContext,
   useCallback,
@@ -9,19 +11,13 @@ import React, {
   useMemo,
   useState,
 } from "react";
-import { useTab, useTabList, useTabPanel } from "react-aria";
-import {
-  Item,
-  TabListState,
-  useCollection,
-  useTabListState,
-} from "react-stately";
-import { ListCollection } from "@react-stately/list";
+import { AriaTabPanelProps, useTab, useTabList, useTabPanel } from "react-aria";
+import { TabListState, useCollection, useTabListState } from "react-stately";
 import { classNames } from "../utilities/css";
 
 import styles from "./TabPanels.module.scss";
 
-type TabPanels = AriaLabelingProps & {
+type TabPanelsProps = AriaLabelingProps & {
   /**
    * The children of the <TabPanels> element. Should include <TabPanels.Tabs>
    * and <TabPanels.Panels> elements.
@@ -57,30 +53,38 @@ type TabPanels = AriaLabelingProps & {
   selectedKey?: Key | null;
 };
 
-type TabPanelsTabs = {
+type TabPanelsTabsProps = AriaLabelingProps & {
   /**
    * The tab items to display. Item keys should match the key of the
    * corresponding <Item> within the <TabPanels.Panels> element.
    */
-  children: ReactNode;
+  children: ItemElement<object> | ItemElement<object>[];
 };
 
-type TabPanelsPanels = {
+type TabPanelsPanelsProps = {
   /**
    * The contents of each tab. Item keys should match the key of the
    * corresponding <Item> within the <TabPanels.Tabs> element.
    */
+  children: ReactElement | ReactElement[];
+};
+
+type TabProps = {
+  item: Node<object>;
+  state: TabListState<object>;
+};
+
+type TabPanelProps = AriaTabPanelProps & {
   children: ReactNode;
+  state: TabListState<object>;
 };
 
 type TabPanelsContextType = {
-  tabListState: TabListState<object>;
+  tabListState: TabListState<object> | null;
   setTabListState: (state: TabListState<object>) => void;
 };
 
-const TabPanelsContext = createContext<TabPanelsContextType<object> | null>(
-  null,
-);
+const TabPanelsContext = createContext<TabPanelsContextType | null>(null);
 
 const useTabPanels = () => {
   const tabPanelsContext = useContext(TabPanelsContext);
@@ -93,7 +97,7 @@ const useTabPanels = () => {
 export function TabPanels(props: TabPanelsProps) {
   const { children } = props;
 
-  const className = classNames(styles.TabPanels, styles[props.orientation]);
+  const className = classNames(styles.TabPanels);
   const [tabListState, setTabListState] = useState<TabListState<object> | null>(
     null,
   );
@@ -109,23 +113,24 @@ export function TabPanels(props: TabPanelsProps) {
   );
 }
 
-function TabPanelTabs(props) {
+function TabPanelTabs(props: TabPanelsTabsProps) {
   const { setTabListState } = useTabPanels();
 
-  let ref = React.useRef(null);
+  const ref = React.useRef(null);
 
-  let state = useTabListState({ ...props, children: props.children });
+  const state = useTabListState({ ...props, children: props.children });
 
-  let { tabListProps } = useTabList(props, state, ref);
+  const { tabListProps } = useTabList(props, state, ref);
 
-  useEffect(() => {
-    setTabListState(state);
-  }, [
-    state.disabledKeys,
-    state.selectedItem,
-    state.selectedKey,
-    props.children,
-  ]);
+  useEffect(
+    () => {
+      setTabListState(state);
+    },
+    // state is too comprehensive of a dependency in and of itself. only change
+    // when specific requirements are met
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [state.disabledKeys, state.selectedItem, state.selectedKey, props.children],
+  );
 
   return (
     <div {...tabListProps} ref={ref}>
@@ -136,10 +141,10 @@ function TabPanelTabs(props) {
   );
 }
 
-function Tab({ item, state }) {
-  let { key, rendered } = item;
-  let ref = React.useRef(null);
-  let { tabProps } = useTab({ key }, state, ref);
+function Tab({ item, state }: TabProps) {
+  const { key, rendered } = item;
+  const ref = React.useRef(null);
+  const { tabProps } = useTab({ key }, state, ref);
   return (
     <div {...tabProps} ref={ref}>
       {rendered}
@@ -147,20 +152,25 @@ function Tab({ item, state }) {
   );
 }
 
-// <TabPanel key={state.selectedItem?.key} state={state} />
-
-function TabPanelPanels(props) {
+function TabPanelPanels(props: TabPanelsPanelsProps) {
   const { tabListState } = useTabPanels();
 
-  const factory = useCallback((nodes) => new ListCollection(nodes), []);
-  const collection = useCollection({ ...props }, factory, {
+  const factory = useCallback(
+    (nodes: Iterable<Node<object>>) => new ListCollection(nodes),
+    [],
+  );
+
+  const collection = useCollection(props, factory, {
     suppressTextValueWarning: true,
   });
+
   const selectedItem = tabListState
     ? collection.getItem(tabListState.selectedKey)
     : null;
 
-  console.log("selected item", selectedItem);
+  if (!tabListState) {
+    return null;
+  }
 
   return (
     <TabPanel {...props} key={tabListState?.selectedKey} state={tabListState}>
@@ -169,9 +179,9 @@ function TabPanelPanels(props) {
   );
 }
 
-function TabPanel({ state, ...props }) {
-  let ref = React.useRef(null);
-  let { tabPanelProps } = useTabPanel(props, state, ref);
+function TabPanel({ state, ...props }: TabPanelProps) {
+  const ref = React.useRef(null);
+  const { tabPanelProps } = useTabPanel(props, state, ref);
   return (
     <div {...tabPanelProps} ref={ref}>
       {props.children}
