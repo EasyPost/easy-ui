050c1a2cc25cd0da92ef1a76b443d271cbf651db
Stephen Watkins <swatkins@easypost.com>
2023-10-27 13:43:51 -0400

    wizard content things

--- easy-ui-react/src/FocusedProductLayout/FocusedProductLayout.stories.tsx
+++ easy-ui-react/src/FocusedProductLayout/FocusedProductLayout.stories.tsx
@@ -1,15 +1,49 @@
+import { action } from "@storybook/addon-actions";
 import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import { Menu } from "../Menu";
 import { Stepper } from "../Stepper";
 import { EasyPostFullLogo, PlaceholderBox } from "../utilities/storybook";
-import { FocusedProductLayout } from "./FocusedProductLayout";
+import {
+  FocusedProductLayout,
+  FocusedProductLayoutProps,
+} from "./FocusedProductLayout";
 
 type Story = StoryObj<typeof FocusedProductLayout>;
+
+const Template = (args: FocusedProductLayoutProps) => (
+  <FocusedProductLayout {...args} />
+);
 
 const meta: Meta<typeof FocusedProductLayout> = {
   title: "Components/ProductLayout/FocusedProductLayout",
   component: FocusedProductLayout,
+  args: {
+    helpMenuItems: [
+      <Menu.Item key="1" href="https://easypost.com" target="_blank">
+        Documentation
+      </Menu.Item>,
+    ],
+    renderBackArrow: (props) => {
+      return (
+        <button
+          {...props}
+          onClick={() => {
+            window.alert("test");
+          }}
+        />
+      );
+    },
+    renderLogo: () => <EasyPostFullLogo />,
+    title: "Page Title",
+    content: (
+      <FocusedProductLayout.Content>
+        <div style={{ marginTop: 24 }}>
+          <PlaceholderBox width="100%">Space for content</PlaceholderBox>
+        </div>
+      </FocusedProductLayout.Content>
+    ),
+  },
   decorators: [
     (Story) => (
       <div className="full-screen-story product-layout-story">
@@ -22,173 +56,76 @@ const meta: Meta<typeof FocusedProductLayout> = {
 export default meta;
 
 export const Default: Story = {
-  render: () => (
-    <FocusedProductLayout
-      helpMenuItems={[
-        <Menu.Item key="1" href="https://easypost.com" target="_blank">
-          Documentation
-        </Menu.Item>,
-      ]}
-      renderBackArrow={(props) => {
-        return (
-          <button
-            {...props}
-            onClick={() => {
-              window.alert("test");
-            }}
-          />
-        );
-      }}
-      renderLogo={() => <EasyPostFullLogo />}
-      title="Page Title"
-      content={
-        <FocusedProductLayout.Content>
-          <div style={{ marginTop: 24 }}>
-            <PlaceholderBox width="100%">Space for content</PlaceholderBox>
-          </div>
-        </FocusedProductLayout.Content>
-      }
-    />
-  ),
+  render: Template.bind({}),
 };
 
 export const WithSidePanel: Story = {
-  render: () => (
-    <FocusedProductLayout
-      helpMenuItems={[
-        <Menu.Item key="1" href="https://easypost.com" target="_blank">
-          Documentation
-        </Menu.Item>,
-      ]}
-      renderBackArrow={(props) => {
-        return <button {...props} />;
-      }}
-      renderLogo={() => <EasyPostFullLogo />}
-      title="Page Title"
-      content={
-        <FocusedProductLayout.Content>
-          <div style={{ marginTop: 24 }}>
-            <PlaceholderBox width="100%">Space for content</PlaceholderBox>
-          </div>
-        </FocusedProductLayout.Content>
-      }
-      sidePanel={
-        <FocusedProductLayout.SidePanel>
-          <PlaceholderBox width="100%">Space for content</PlaceholderBox>
-        </FocusedProductLayout.SidePanel>
-      }
-    />
-  ),
+  render: Template.bind({}),
+  args: {
+    sidePanel: (
+      <FocusedProductLayout.SidePanel>
+        <PlaceholderBox width="100%">Space for content</PlaceholderBox>
+      </FocusedProductLayout.SidePanel>
+    ),
+  },
 };
 
 export const WithSidePanelAtStart: Story = {
-  render: () => (
-    <FocusedProductLayout
-      helpMenuItems={[
-        <Menu.Item key="1" href="https://easypost.com" target="_blank">
-          Documentation
-        </Menu.Item>,
-      ]}
-      renderBackArrow={(props) => {
-        return <button {...props} />;
-      }}
-      renderLogo={() => <EasyPostFullLogo />}
-      title="Page Title"
-      content={
-        <FocusedProductLayout.Content>
-          <div style={{ marginTop: 24 }}>
-            <PlaceholderBox width="100%">Space for content</PlaceholderBox>
-          </div>
-        </FocusedProductLayout.Content>
-      }
-      sidePanel={
-        <FocusedProductLayout.SidePanel>
-          <PlaceholderBox width="100%">Space for content</PlaceholderBox>
-        </FocusedProductLayout.SidePanel>
-      }
-      sidePanelPosition="start"
-    />
-  ),
+  render: Template.bind({}),
+  args: {
+    sidePanel: (
+      <FocusedProductLayout.SidePanel>
+        <PlaceholderBox width="100%">Space for content</PlaceholderBox>
+      </FocusedProductLayout.SidePanel>
+    ),
+    sidePanelPosition: "start",
+  },
 };
 
 export const WithSidePanelWidth: Story = {
-  render: () => (
-    <FocusedProductLayout
-      helpMenuItems={[
-        <Menu.Item key="1" href="https://easypost.com" target="_blank">
-          Documentation
-        </Menu.Item>,
-      ]}
-      renderBackArrow={(props) => {
-        return <button {...props} />;
-      }}
-      renderLogo={() => <EasyPostFullLogo />}
-      title="Page Title"
-      content={
-        <FocusedProductLayout.Content>
-          <div style={{ marginTop: 24 }}>
-            <PlaceholderBox width="100%">Space for content</PlaceholderBox>
-          </div>
-        </FocusedProductLayout.Content>
-      }
-      sidePanel={
-        <FocusedProductLayout.SidePanel width="50%">
-          <PlaceholderBox width="100%">Space for content</PlaceholderBox>
-        </FocusedProductLayout.SidePanel>
-      }
-      sidePanelPosition="start"
-    />
-  ),
+  render: Template.bind({}),
+  args: {
+    sidePanel: (
+      <FocusedProductLayout.SidePanel width="50%">
+        <PlaceholderBox width="100%">Space for content</PlaceholderBox>
+      </FocusedProductLayout.SidePanel>
+    ),
+  },
 };
 
 export const Wizard: Story = {
-  render: () => (
-    <FocusedProductLayout
-      helpMenuItems={[
-        <Menu.Item key="1" href="https://easypost.com" target="_blank">
-          Documentation
-        </Menu.Item>,
-      ]}
-      renderBackArrow={(props) => {
-        return <button {...props} />;
-      }}
-      renderLogo={() => <EasyPostFullLogo />}
-      title="Page Title"
-      content={
-        <FocusedProductLayout.WizardContent
-          stepper={
-            <Stepper activeStepIndex={0}>
-              {["Step 1", "Step 2", "Step 3", "Step 4"].map((step, index) => (
-                <Stepper.Step
-                  key={step}
-                  stepIndex={index}
-                  onPress={() => {}}
-                  isComplete={false}
-                  isAccessible={false}
-                >
-                  {step}
-                </Stepper.Step>
-              ))}
-            </Stepper>
-          }
-          previousAction={{
-            content: "Back",
-            onAction: () => {
-              window.alert("test");
-            },
-          }}
-          nextAction={{
-            content: "Next",
-            onAction: () => {
-              window.alert("next");
-            },
-          }}
-        >
-          <div style={{ marginTop: 24 }}>
-            <PlaceholderBox width="100%">Space for content</PlaceholderBox>
-          </div>
-        </FocusedProductLayout.WizardContent>
-      }
-    />
-  ),
+  render: Template.bind({}),
+  args: {
+    content: (
+      <FocusedProductLayout.WizardContent
+        stepper={
+          <Stepper activeStepIndex={1}>
+            {["Step 1", "Step 2", "Step 3", "Step 4"].map((step, index) => (
+              <Stepper.Step
+                key={step}
+                stepIndex={index}
+                onPress={() => {}}
+                isComplete={index < 1}
+                isAccessible={false}
+              >
+                {step}
+              </Stepper.Step>
+            ))}
+          </Stepper>
+        }
+        previousAction={{
+          content: "Back",
+          onAction: action("previousAction.onAction pressed"),
+        }}
+        nextAction={{
+          content: "Next",
+          onAction: action("nextAction.onAction pressed"),
+        }}
+      >
+        <div style={{ marginTop: 24 }}>
+          <PlaceholderBox width="100%">Space for content</PlaceholderBox>
+        </div>
+      </FocusedProductLayout.WizardContent>
+    ),
+  },
 };
