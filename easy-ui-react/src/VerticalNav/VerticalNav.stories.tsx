644b653f89d4fc21f4e4cc6e22ee5b11ebf8e80f
Stephen Watkins <swatkins@easypost.com>
2023-11-13 11:18:08 -0500

    add simple and dense stories

--- easy-ui-react/src/VerticalNav/VerticalNav.stories.tsx
+++ easy-ui-react/src/VerticalNav/VerticalNav.stories.tsx
@@ -18,7 +18,165 @@ const meta: Meta<typeof ExpandableVerticalNav> = {
 
 export default meta;
 
-export const Default: Story = {
+export const Simple: Story = {
+  render: () => {
+    /* eslint-disable react-hooks/rules-of-hooks */
+    const [page, setPage] = useState("1");
+    const commitPage = useCallback((newPage: string) => {
+      setPage(newPage);
+    }, []);
+    /* eslint-enable react-hooks/rules-of-hooks */
+    return (
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div
+          style={{
+            width: 215,
+            height: "calc(100svh - 2rem)",
+            background: "#f1f1f1",
+          }}
+        >
+          <VerticalNav
+            renderLogo={() => <EasyPostFullLogo />}
+            selectedKeys={[page.substring(0, 1)]}
+          >
+            <VerticalNav.Item
+              key="1"
+              iconSymbol={MenuBookIcon}
+              label="Item 1"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("1")}
+            />
+            <VerticalNav.Item
+              key="2"
+              iconSymbol={MenuBookIcon}
+              label="Item 2"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("2")}
+            />
+            <VerticalNav.Item
+              key="3"
+              iconSymbol={LocalShippingIcon}
+              label="Item 3"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("3")}
+            />
+            <VerticalNav.Item
+              key="4"
+              iconSymbol={MenuBookIcon}
+              label="Item 4"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("4")}
+            />
+            <VerticalNav.Item
+              key="5"
+              iconSymbol={LocalShippingIcon}
+              label="Item 5"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("5")}
+            />
+          </VerticalNav>
+        </div>
+        <div style={{ padding: 48 }}>{page ? <>Page {page}</> : null}</div>
+      </div>
+    );
+  },
+};
+
+export const Dense: Story = {
+  render: () => {
+    /* eslint-disable react-hooks/rules-of-hooks */
+    const [page, setPage] = useState("1");
+    const commitPage = useCallback((newPage: string) => {
+      setPage(newPage);
+    }, []);
+    /* eslint-enable react-hooks/rules-of-hooks */
+    return (
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div
+          style={{
+            width: 215,
+            height: "calc(100svh - 2rem)",
+            background: "#f1f1f1",
+          }}
+        >
+          <VerticalNav
+            renderLogo={() => <EasyPostFullLogo />}
+            selectedKeys={[page.substring(0, 1)]}
+          >
+            <VerticalNav.Item
+              key="1"
+              iconSymbol={MenuBookIcon}
+              label="Item 1"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("1")}
+            />
+            <VerticalNav.Item
+              key="2"
+              iconSymbol={MenuBookIcon}
+              label="Item 2"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("2")}
+            >
+              <VerticalNav.Subnav selectedKeys={[page.substring(0, 3)]}>
+                <VerticalNav.Item
+                  key="2/a"
+                  label="Subitem a"
+                  as={FakeClientSideRouterLink}
+                  onClick={() => commitPage("2/a")}
+                />
+                <VerticalNav.Item
+                  key="2/b"
+                  label="Subitem b"
+                  as={FakeClientSideRouterLink}
+                  onClick={() => commitPage("2/b")}
+                />
+              </VerticalNav.Subnav>
+            </VerticalNav.Item>
+            <VerticalNav.Item
+              key="3"
+              iconSymbol={LocalShippingIcon}
+              label="Item 3"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("3")}
+            >
+              <VerticalNav.Subnav selectedKeys={[page]}>
+                <VerticalNav.Item
+                  key="3/a"
+                  label="Subitem a"
+                  as={FakeClientSideRouterLink}
+                  onClick={() => commitPage("3/a")}
+                />
+                <VerticalNav.Item
+                  key="3/b"
+                  label="Subitem b"
+                  as={FakeClientSideRouterLink}
+                  onClick={() => commitPage("3/b")}
+                />
+              </VerticalNav.Subnav>
+            </VerticalNav.Item>
+            <VerticalNav.Item
+              key="4"
+              iconSymbol={MenuBookIcon}
+              label="Item 4"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("4")}
+            />
+            <VerticalNav.Item
+              key="5"
+              iconSymbol={LocalShippingIcon}
+              label="Item 5"
+              as={FakeClientSideRouterLink}
+              onClick={() => commitPage("5")}
+            />
+          </VerticalNav>
+        </div>
+        <div style={{ padding: 48 }}>{page ? <>Page {page}</> : null}</div>
+      </div>
+    );
+  },
+};
+
+export const Expandable: Story = {
   render: () => {
     /* eslint-disable react-hooks/rules-of-hooks */
     const [page, setPage] = useState("1");
