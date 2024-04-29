a7d2c9bd97334930b3b8fbf9d21ad2a73338d0f2
Stephen Watkins <swatkins@easypost.com>
2024-04-29 11:29:46 -0400

    upgrade all storybook pkgs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   addons: [
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
   ],
   docs: {
     autodocs: true,
