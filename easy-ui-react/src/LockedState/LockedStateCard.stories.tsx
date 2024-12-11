b875211af42bcc71385d84d9d5eb4fbf10bb356b
Bianca Hale <bhale@easypost.com>
2024-12-10 22:08:07 -0600

    Create LockedState component

---
+++ easy-ui-react/src/LockedState/LockedStateCard.stories.tsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { Meta, StoryObj } from "@storybook/react";
+// import { LockedStateCard, LockedStateCardProps } from "./LockedStateCard";
+// import { Button } from "../Button";
+// import { PlaceholderBox } from "../utilities/storybook";
+
+// type Story = StoryObj<typeof LockedStateCard>;
+
+// const Template = (args: LockedStateCardProps) => {
+//   const { children, ...restArgs } = args;
+//   return <LockedStateCard {...restArgs}>{children}</LockedStateCard>;
+// };
+
+// const meta: Meta<typeof LockedStateCard> = {
+//   title: "Components/Cards/LockedStateCard",
+//   component: LockedStateCard,
+// };
+
+// export default meta;
+
+// export const Simple: Story = {};
---
+++ easy-ui-react/src/LockedState/LockedStateCard.tsx
@@ -0,0 +1,8 @@
+// import React, { ReactNode } from "react";
+// import { Text, TextProps } from "../Text";
+// import { Card, CardProps } from "../Card";
+// import { Icon, IconSymbol } from "../Icon";
+// import { Button, ButtonProps } from "../Button";
+// import { VerticalStack, VerticalStackProps } from "../VerticalStack";
+// import { HorizontalStack, HorizontalStackProps } from "../HorizontalStack";
+// import { classNames } from "../utilities/css";
