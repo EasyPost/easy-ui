5fd2f4f9fe1de588b23ac407b35d54f10b0222d6
Stephen Watkins <swatkins@easypost.com>
2025-01-07 17:31:12 -0500

    wip

---
+++ easy-ui-react/src/MultiSelect/MultiSelect.stories.tsx
@@ -0,0 +1,69 @@
+import { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { useListData } from "react-stately";
+import { FedExLogoImg, InlineStoryDecorator } from "../utilities/storybook";
+import { MultipleSelect, SelectedKey } from "./MultiSelect";
+
+type Story = StoryObj<typeof MultipleSelect>;
+
+const meta: Meta<typeof MultipleSelect> = {
+  title: "Components/MultiSelect",
+  component: MultipleSelect,
+  args: {},
+  decorators: [InlineStoryDecorator],
+};
+
+export default meta;
+
+export const Default: Story = {
+  render: () => <Component />,
+};
+
+function Component() {
+  const selectedItems = useListData<SelectedKey>({
+    initialItems: [fruits[0], fruits[1]],
+  });
+  return (
+    <div style={{ display: "inline-flex", width: "100%", maxWidth: 600 }}>
+      <MultipleSelect
+        label="Fruits"
+        selectedItems={selectedItems}
+        items={fruits}
+        placeholder="Select a fruit"
+        maxItemsUntilScroll={10}
+        pill={(item) => (
+          <MultipleSelect.Pill icon={item.icon} label={item.label} />
+        )}
+      >
+        {(item) => (
+          <MultipleSelect.Option textValue={item.label}>
+            <MultipleSelect.OptionText>{item.label}</MultipleSelect.OptionText>
+          </MultipleSelect.Option>
+        )}
+      </MultipleSelect>
+    </div>
+  );
+}
+
+const fruits = [
+  { id: 1, label: "Apple", icon: FedExLogoImg },
+  { id: 2, label: "Banana" },
+  { id: 3, label: "Cherry", icon: FedExLogoImg },
+  { id: 4, label: "Date", icon: FedExLogoImg },
+  { id: 5, label: "Elderberry", icon: FedExLogoImg },
+  { id: 6, label: "Fig", icon: FedExLogoImg },
+  { id: 7, label: "Grape", icon: FedExLogoImg },
+  { id: 8, label: "Honeydew", icon: FedExLogoImg },
+  { id: 9, label: "Kiwi", icon: FedExLogoImg },
+  { id: 10, label: "Lemon", icon: FedExLogoImg },
+  { id: 11, label: "Mango", icon: FedExLogoImg },
+  { id: 12, label: "Nectarine", icon: FedExLogoImg },
+  { id: 13, label: "Orange", icon: FedExLogoImg },
+  { id: 14, label: "Papaya", icon: FedExLogoImg },
+  { id: 15, label: "Quince", icon: FedExLogoImg },
+  { id: 16, label: "Raspberry", icon: FedExLogoImg },
+  { id: 17, label: "Strawberry", icon: FedExLogoImg },
+  { id: 18, label: "Tangerine", icon: FedExLogoImg },
+  { id: 19, label: "Ugli Fruit", icon: FedExLogoImg },
+  { id: 20, label: "Watermelon", icon: FedExLogoImg },
+] as const as SelectedKey[];
---
+++ easy-ui-react/src/MultiSelect/MultiSelect.tsx
@@ -0,0 +1,328 @@
+import KeyboardArrowDownIcon from "@easypost/easy-ui-icons/KeyboardArrowDown";
+import React, {
+  ReactNode,
+  RefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { useFilter, VisuallyHidden } from "react-aria";
+import {
+  Button,
+  ComboBox,
+  ComboBoxProps as ComboBoxPrimitiveProps,
+  Input,
+  Key,
+  ListBox,
+  ListBoxItem,
+  ListBoxItemProps,
+  Popover,
+  ValidationResult,
+} from "react-aria-components";
+import { ListData, useListData } from "react-stately";
+import { Icon } from "../Icon";
+import { MenuOverlayProps } from "../Menu/MenuOverlay";
+import {
+  DEFAULT_MAX_ITEMS_UNTIL_SCROLL,
+  getMenuPopoverMaxHeight,
+  ITEM_HEIGHT,
+  Y_PADDING_INSIDE_OVERLAY,
+} from "../Menu/utilities";
+import { PillGroup, PillProps } from "../PillGroup";
+import { Text, TextProps } from "../Text";
+import { classNames, getComponentToken, pxToRem } from "../utilities/css";
+
+import styles from "./MultiSelect.module.scss";
+import { useScrollbar } from "../utilities/useScrollbar";
+
+type SelectedKey = {
+  id: Key;
+} & PillProps;
+
+type MultipleSelectProps<T extends object> = Omit<
+  ComboBoxPrimitiveProps<T>,
+  | "children"
+  | "validate"
+  | "allowsEmptyCollection"
+  | "inputValue"
+  | "selectedKey"
+  | "className"
+  | "value"
+  | "onSelectionChange"
+  | "onInputChange"
+> & {
+  label: string;
+  placeholder?: string;
+  items: Array<T>;
+  selectedItems: ListData<T>;
+  className?: string;
+  onItemInserted?: (key: Key) => void;
+  onItemCleared?: (key: Key) => void;
+  renderEmptyState?: (inputValue: string) => React.ReactNode;
+  pill: (item: T) => React.ReactNode;
+  children: React.ReactNode | ((item: T) => React.ReactNode);
+  errorMessage?: string | ((validation: ValidationResult) => string);
+  maxItemsUntilScroll?: MenuOverlayProps<T>["maxItemsUntilScroll"];
+};
+
+const MultipleSelect = <T extends SelectedKey>({
+  children,
+  items,
+  label,
+  onItemCleared,
+  onItemInserted,
+  pill,
+  selectedItems,
+  maxItemsUntilScroll = DEFAULT_MAX_ITEMS_UNTIL_SCROLL,
+  ...props
+}: MultipleSelectProps<T>) => {
+  const triggerRef = useRef<HTMLDivElement | null>(null);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const [width, setWidth] = useState(0);
+
+  const { contains } = useFilter({ sensitivity: "base" });
+  const selectedKeys = selectedItems.items.map((i) => i.id);
+
+  const filter = useCallback(
+    (item: T, filterText: string) => {
+      return (
+        !selectedKeys.includes(item.id) && contains(item.label, filterText)
+      );
+    },
+    [contains, selectedKeys],
+  );
+
+  const accessibleList = useListData({
+    initialItems: items,
+    filter,
+  });
+
+  const [fieldState, setFieldState] = useState<{
+    selectedKey: Key | null;
+    inputValue: string;
+  }>({
+    selectedKey: null,
+    inputValue: "",
+  });
+
+  const onRemove = useCallback(
+    (keys: Set<Key>) => {
+      const key = keys.values().next().value;
+      if (key) {
+        selectedItems.remove(key);
+        setFieldState({
+          inputValue: "",
+          selectedKey: null,
+        });
+        onItemCleared?.(key);
+      }
+    },
+    [selectedItems, onItemCleared],
+  );
+
+  const onSelectionChange = (id: Key | null) => {
+    if (!id) {
+      return;
+    }
+
+    const item = accessibleList.getItem(id);
+
+    if (!item) {
+      return;
+    }
+
+    if (!selectedKeys.includes(id)) {
+      selectedItems.append(item);
+      setFieldState({
+        inputValue: "",
+        selectedKey: id,
+      });
+      onItemInserted?.(id);
+    }
+
+    accessibleList.setFilterText("");
+  };
+
+  const onInputChange = (value: string) => {
+    setFieldState((prev) => ({
+      inputValue: value,
+      selectedKey: value === "" ? null : prev.selectedKey,
+    }));
+
+    accessibleList.setFilterText(value);
+  };
+
+  const popLast = useCallback(() => {
+    if (selectedItems.items.length === 0) {
+      return;
+    }
+
+    const endKey = selectedItems.items[selectedItems.items.length - 1];
+
+    if (endKey) {
+      selectedItems.remove(endKey.id);
+      onItemCleared?.(endKey.id);
+    }
+
+    setFieldState({
+      inputValue: "",
+      selectedKey: null,
+    });
+  }, [selectedItems, onItemCleared]);
+
+  const onKeyDownCapture = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Backspace" && fieldState.inputValue === "") {
+        popLast();
+      }
+    },
+    [popLast, fieldState.inputValue],
+  );
+
+  useEffect(() => {
+    const trigger = triggerRef.current;
+    if (!trigger) return;
+
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        setWidth(entry.target.clientWidth);
+      }
+    });
+
+    observer.observe(trigger);
+    return () => {
+      observer.unobserve(trigger);
+    };
+  }, []);
+
+  const triggerButtonRef = useRef<HTMLButtonElement | null>(null);
+
+  return (
+    <div
+      ref={triggerRef}
+      className={classNames(
+        styles.MultiSelect,
+        props.isDisabled && styles.disabled,
+      )}
+    >
+      {selectedItems.items.length > 0 && (
+        <PillGroup
+          items={selectedItems.items}
+          horizontalStackContainerProps={{ gap: "1" }}
+          onRemove={onRemove}
+          label={label}
+        >
+          {pill}
+        </PillGroup>
+      )}
+      <div className={styles.comboBoxContainer}>
+        <ComboBox
+          {...props}
+          className={styles.comboBox}
+          allowsEmptyCollection
+          aria-label="Available items"
+          items={accessibleList.items}
+          selectedKey={fieldState.selectedKey}
+          inputValue={fieldState.inputValue}
+          onSelectionChange={onSelectionChange}
+          onInputChange={onInputChange}
+        >
+          <div className={styles.inputContainer}>
+            <Input
+              placeholder={props.placeholder}
+              className={styles.input}
+              onBlur={() => {
+                setFieldState({ inputValue: "", selectedKey: null });
+                accessibleList.setFilterText("");
+              }}
+              onKeyDownCapture={onKeyDownCapture}
+            />
+            <VisuallyHidden>
+              <Button
+                slot="remove"
+                type="button"
+                aria-label="Remove"
+                ref={triggerButtonRef}
+              />
+            </VisuallyHidden>
+          </div>
+          <Popover
+            isNonModal
+            className={styles.popover}
+            style={{
+              width,
+              ...getComponentToken(
+                "menu",
+                "item_height",
+                `${pxToRem(ITEM_HEIGHT)}rem`,
+              ),
+              ...getComponentToken(
+                "menu",
+                "padding.y",
+                `${pxToRem(Y_PADDING_INSIDE_OVERLAY)}rem`,
+              ),
+            }}
+            triggerRef={triggerRef}
+            scrollRef={menuRef}
+            trigger="ComboBox"
+            maxHeight={getMenuPopoverMaxHeight({ maxItemsUntilScroll })}
+          >
+            <ListBoxContainer menuRef={menuRef}>
+              <ListBox
+                renderEmptyState={() => (
+                  <Text variant="subtitle2" color="neutral.400">
+                    No results found
+                  </Text>
+                )}
+                selectionMode="multiple"
+              >
+                {children}
+              </ListBox>
+            </ListBoxContainer>
+          </Popover>
+        </ComboBox>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => triggerButtonRef.current?.click()}
+          tabIndex={-1}
+          aria-hidden="true"
+        >
+          <Icon symbol={KeyboardArrowDownIcon} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+function ListBoxContainer({
+  menuRef,
+  children,
+}: {
+  menuRef: RefObject<HTMLDivElement>;
+  children: ReactNode;
+}) {
+  useScrollbar(menuRef, "ezui-os-theme-overlay");
+  return (
+    <div className={styles.menu} ref={menuRef}>
+      {children}
+    </div>
+  );
+}
+
+function MultipleSelectOption(props: ListBoxItemProps) {
+  return <ListBoxItem {...props} className={styles.option} />;
+}
+
+function MultipleSelectOptionText(props: TextProps) {
+  return <Text variant="body1" {...props} />;
+}
+
+MultipleSelect.Pill = PillGroup.Pill;
+MultipleSelect.Option = MultipleSelectOption;
+MultipleSelect.OptionText = MultipleSelectOptionText;
+
+export { MultipleSelect };
+export type { MultipleSelectProps, SelectedKey };
