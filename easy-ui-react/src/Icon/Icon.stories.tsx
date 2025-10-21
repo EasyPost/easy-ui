c384358e45afd75827a5af0b9d30fa1534e3a236
Stephen Watkins <swatkins@easypost.com>
2025-10-21 16:21:06 -0400

    feat: Icon gallery (#1844)
    ## 📝 Changes
    
    - Adds gallery of icons based on Alex's work
    - Autoloads them
    
    
    https://63f50c7c86f6514d2e0ef4be-eyyvwmczhw.chromatic.com/?path=/story/primitives-icon--gallery
    
    ## ✅ Checklist
    
    Easy UI has certain UX standards that must be met. In general,
    non-trivial changes should meet the following criteria:
    
    - [ ] Visuals match Design Specs in Figma
    - [x] Stories accompany any component changes
    - [ ] Code is in accordance with our style guide
    - [ ] Design tokens are utilized
    - [ ] Unit tests accompany any component changes
    - [ ] TSDoc is written for any API surface area
    - [ ] Specs are up-to-date
    - [ ] Console is free from warnings
    - [ ] No accessibility violations are reported
    - [ ] Cross-browser check is performed (Chrome, Safari, Firefox)
    - [ ] Changeset is added
    
    ~Strikethrough~ any items that are not applicable to this pull request.
    
    ---------
    
    Co-authored-by: Alex Matthews <amatthews@easypost.com>

--- easy-ui-react/src/Icon/Icon.stories.tsx
+++ easy-ui-react/src/Icon/Icon.stories.tsx
@@ -4,12 +4,37 @@ import InfoIcon from "@easypost/easy-ui-icons/Info";
 import WarningIcon from "@easypost/easy-ui-icons/Warning";
 import { Meta, StoryObj } from "@storybook/react-vite";
 import React from "react";
+import { HorizontalStack } from "../HorizontalStack";
+import { IconSymbol } from "../types";
 import {
   createColorTokensControl,
   createLabelledOptionsControl,
   FedExLogoImg,
 } from "../utilities/storybook";
+import { VerticalStack } from "../VerticalStack";
 import { Icon, IconProps } from "./Icon";
+
+type IconModule = { default: IconSymbol };
+type IconGalleryItem = { name: string; Component: IconSymbol };
+
+const iconModules: Record<string, IconModule> = import.meta.glob(
+  "../../../easy-ui-icons/dist/*.mjs",
+  { eager: true },
+);
+
+const icons = Object.entries(iconModules)
+  .map(([path, iconModule]) => {
+    const { default: Component } = iconModule;
+    if (!Component) {
+      return null;
+    }
+    const name = path
+      .split("/")
+      .pop()!
+      .replace(/\.mjs$/, "");
+    return { name, Component };
+  })
+  .filter(Boolean) as IconGalleryItem[];
 
 type Story = StoryObj<typeof Icon>;
 
@@ -18,7 +43,6 @@ const Template = (args: IconProps) => <Icon {...args} />;
 const meta: Meta<typeof Icon> = {
   title: "Primitives/Icon",
   argTypes: {
-    // TODO: Figure out how to include all icons from our icons project
     symbol: createLabelledOptionsControl({
       CheckCircle: CheckCircleIcon,
       Info: InfoIcon,
@@ -55,4 +79,16 @@ export const Controls: Story = {
   args: {
     symbol: CheckCircleIcon,
   },
+};
+
+export const Gallery: Story = {
+  render: () => (
+    <VerticalStack gap="1">
+      {icons.map(({ name, Component }) => (
+        <HorizontalStack key={name} blockAlign="center" gap="2">
+          <Icon symbol={Component} /> {name}
+        </HorizontalStack>
+      ))}
+    </VerticalStack>
+  ),
 };
